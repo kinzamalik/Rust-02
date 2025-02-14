2e0ec686bdd020ef8f8cc7cea2be97d8be8382c5
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-14 15:54:15 +0000

    if else demo

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -71,3 +71,12 @@
 
 
 // if else expressions
+
+fn main(){
+    let age = 15;
+    if age >=18 {
+        println!("you can attend summit")
+    }else{
+        println!("you cannot attend")
+    }
+}
