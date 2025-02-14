deb75113fcf2e73c89bec0c5f8b3f266fddc5d91
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-14 15:22:22 +0000

    Arthematic opertaions

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -25,3 +25,37 @@
 // }
 
 
+// Arthematic operations
+
+
+
+fn main() {
+    let a = 10;
+    let b = 5;
+
+    // Addition
+    let sum = a + b;
+    println!("Addition: {} + {} = {}", a, b, sum);
+
+    // Subtraction
+    let difference = a - b;
+    println!("Subtraction: {} - {} = {}", a, b, difference);
+
+    // Multiplication
+    let product = a * b;
+    println!("Multiplication: {} * {} = {}", a, b, product);
+
+    // Division
+    let quotient = a / b; // Integer division
+    println!("Division: {} / {} = {}", a, b, quotient);
+
+    // Remainder (Modulo)
+    let remainder = a % b;
+    println!("Remainder: {} % {} = {}", a, b, remainder);
+}
+
+
+
+
+
+
