98bd743e6c7520b222f96bab120fe6caa67a6eda
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-14 15:37:15 +0000

    Sum Function

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -29,32 +29,43 @@
 
 
 
-fn main() {
-    let a = 10;
-    let b = 5;
+// fn main() {
+//     let a = 10;
+//     let b = 5;
 
-    // Addition
-    let sum = a + b;
-    println!("Addition: {} + {} = {}", a, b, sum);
+//     // Addition
+//     let sum = a + b;
+//     println!("Addition: {} + {} = {}", a, b, sum);
 
-    // Subtraction
-    let difference = a - b;
-    println!("Subtraction: {} - {} = {}", a, b, difference);
+//     // Subtraction
+//     let difference = a - b;
+//     println!("Subtraction: {} - {} = {}", a, b, difference);
 
-    // Multiplication
-    let product = a * b;
-    println!("Multiplication: {} * {} = {}", a, b, product);
+//     // Multiplication
+//     let product = a * b;
+//     println!("Multiplication: {} * {} = {}", a, b, product);
 
-    // Division
-    let quotient = a / b; // Integer division
-    println!("Division: {} / {} = {}", a, b, quotient);
+//     // Division
+//     let quotient = a / b; // Integer division
+//     println!("Division: {} / {} = {}", a, b, quotient);
 
-    // Remainder (Modulo)
-    let remainder = a % b;
-    println!("Remainder: {} % {} = {}", a, b, remainder);
+//     // Remainder (Modulo)
+//     let remainder = a % b;
+//     println!("Remainder: {} % {} = {}", a, b, remainder);
+// }
+
+fn sum(a:i32, b:i32) -> i32 {
+    a + b
 }
 
+fn display_result(result:i32){
+    println!("{:?}", result);
+}
 
+fn main (){
+let result = sum(3 , 4);
+display_result(result)
+}
 
 
 
