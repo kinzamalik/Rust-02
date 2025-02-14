458b00334126fbe9ac26dfd8aa8716e64a73ea20
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-14 15:06:51 +0000

    functions

---
+++ helloworld/src/main.rs
@@ -0,0 +1,27 @@
+// Entry point of the application
+
+// fn main() {
+//     // Displaying massage to the user.
+//     println!("Hello, world!");
+// }
+
+
+
+// Functions
+
+// Display your first and last name
+
+// fn first_name(){
+//     println!("Kinza")
+// }
+
+// fn last_name(){
+//     println!("Malik")
+// }
+
+// fn main(){
+//     first_name();
+//     last_name();
+// }
+
+
