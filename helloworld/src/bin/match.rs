120e8cdabd2f9384d3e5b5c9c4b3c7d15543bed1
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-15 11:21:03 +0000

    Match

--- helloworld/src/bin/match.rs
+++ helloworld/src/bin/match.rs
@@ -1,9 +1,41 @@
-fn main(){
-    let my_name = "Kinza";
-    match my_name{
-        "Kinza" => println!("That's my name"),
-        "Alice" => println!("That's not my name"),
-        "Bob" => println!("Hello Bob!"),
-        _ => println!("Somthin else")
-    }
-}
+// fn main(){
+//     let my_name = "Kinza";
+//     match my_name{
+//         "Kinza" => println!("That's my name"),
+//         "Alice" => println!("That's not my name"),
+//         "Bob" => println!("Hello Bob!"),
+//         _ => println!("Somthin else")
+//     }
+// }
+
+
+
+
+
+
+
+
+// fn main(){
+//     let my_bool = false;
+    
+
+//     match my_bool {
+//         true => println!("It's true"),
+//         false => println!("It's false"),
+//     }
+// }
+
+
+// fn main(){
+//     let my_number = 2;
+
+//     match my_number {
+//         1 => println!("One"),
+//         2 => println!("Two"),
+//         3 => println!("Three"),
+//         _ => println!("Ther"),
+
+//     }
+// }
+
+
