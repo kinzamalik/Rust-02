46e696ae33233cfc29084ff5574f90fd1f65a183
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-17 06:07:57 +0000

    enum color example

--- helloworld/src/bin/enum.rs
+++ helloworld/src/bin/enum.rs
@@ -1,16 +1,36 @@
-enum Direction{
-    Left,
-    Right,
-    Up,
-    Down,
+// enum Direction{
+//     Left,
+//     Right,
+//     Up,
+//     Down,
+// }
+
+// fn main(){
+//     let go = Direction::Left;
+//     match go {
+//         Direction::Left => println!("Left"),
+//         Direction::Right => println!("Right"),
+//         Direction::Up => println!("Up"),
+//         Direction::Down => println!("Down")
+//     }
+// }
+
+
+
+enum Color{
+    Red,
+    Blue,
+    Yellow
+}
+
+fn print_color(my_color:Color){
+    match my_color{
+        Color::Red => println!("Red"),
+        Color::Blue => println!("Blue"),
+        Color::Yellow => println!("Yellow"),
+    }
 }
 
 fn main(){
-    let go = Direction::Left;
-    match go {
-        Direction::Left => println!("Left"),
-        Direction::Right => println!("Right"),
-        Direction::Up => println!("Up"),
-        Direction::Down => println!("Down")
-    }
+    print_color(Color::Blue);
 }
