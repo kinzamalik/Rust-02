9d1130c486448a109c215d1f129c5979336cb7a4
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-17 05:11:47 +0000

    while loop

--- helloworld/src/bin/repetition.rs
+++ helloworld/src/bin/repetition.rs
@@ -1,11 +1,22 @@
-fn main(){
-    let mut n = 1;
+// fn main(){
+//     let mut n = 1;
 
-    loop{
-        println!("{:?}",n);
-        if n == 4{
-            break;
-        }
-        n = n + 1;
+//     loop{
+//         println!("{:?}",n);
+//         if n == 4{
+//             break;
+//         }
+//         n = n + 1;
+//     }
+// }
+
+// while loop
+
+fn main(){
+    let mut i = 1;
+
+    while i <= 4 {
+        println!{"{:?}",i}
+        i = i + 1;
     }
 }
