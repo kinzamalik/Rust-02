71d561512bd91d6fcd0d49b7877aed375e30d62b
Kinza Malik <53088154+kinzamalik@users.noreply.github.com>
2025-02-17 05:20:40 +0000

    reverse counter

--- helloworld/src/bin/repetition.rs
+++ helloworld/src/bin/repetition.rs
@@ -12,11 +12,24 @@
 
 // while loop
 
-fn main(){
-    let mut i = 1;
+// fn main(){
+//     let mut i = 1;
 
-    while i <= 4 {
-        println!{"{:?}",i}
-        i = i + 1;
+//     while i <= 4 {
+//         println!{"{:?}",i}
+//         i = i + 1;
+//     }
+// }
+
+
+// Counter 5 - 1;
+
+fn main(){
+    let mut counter = 5;
+
+    while counter >= 1{
+        println!("{:?}",counter);
+        counter = counter - 1;
     }
+    println!("Done!")
 }
