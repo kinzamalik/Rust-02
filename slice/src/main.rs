ab4bb245f6a1756e738dcb561150fca48340f63b
Kinza <kiinza.malik@gmail.com>
2025-02-26 11:07:59 +0500

    overflow variable

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     // println!("Hello, world!");
-    let apple: i32 = 3;
+    let mut  apple: i8 = 10;
+
+    for i in 0..1000{
+
+        apple = apple + 100;
+
+    }
+    print!("apple {}", apple)
 }
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+C:\Users\Dell\Desktop\Rust\Rust-02\slice\target\debug\slice.exe: C:\Users\Dell\Desktop\Rust\Rust-02\slice\src\main.rs
